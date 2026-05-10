eca47b0848437dab0645442d0a8e0ab5712b4740
hasnainbanusi <hasnain10.official@gmail.com>
2026-05-10 17:29:37 +0500

    hasnain

--- day01/s.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#this is second file
-
-echo "hello world this is my second file in shell scripting for devops"
-
-echo "ok , we understand this for you"
-
-echo "thanks for your time"
