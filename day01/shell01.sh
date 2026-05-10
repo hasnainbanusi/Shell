45722a7e5e72dd72ea81e081319d39bf8e0315a0
hasnainbanusi <hasnain10.official@gmail.com>
2026-05-10 16:59:12 +0500

    hasnain

---
+++ day01/shell01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#This is my first shell command and file in linux
+
+echo "Hello dosto, this is the first day of shell scripting for DevOps"
+
+echo "This is the echo command where the input is displayed on the console or on the screen"
+
+echo "keep watching"
+
