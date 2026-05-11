be967d5587a95747ee43dd3062508e691413d948
hasnainbanusi <hasnain10.official@gmail.com>
2026-05-11 10:33:57 +0500

    hasnain

--- day02/new_user.sh
+++ day02/new_user.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 #How to add New user via shebang file
-
+<<comment
 read -p "Enter user name " username
 
 sudo useradd -m $username
 
 echo "New user added"
+comment
+
+#How to pass argument in code
+
+echo "I'm studying BS $1 and Specializing is in $2"
+
