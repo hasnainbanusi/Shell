29cc4ecad0c21184ca98e478f5dd5d20ea943871
hasnainbanusi <hasnain10.official@gmail.com>
2026-05-14 16:23:26 +0500

    hasnain

---
+++ day02/f.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#this is for practice
+<< hem
+i=0
+while [[ $(($i%2))==0 && $i -le 30 ]]
+do
+	echo "Prime No: $i"
+	i=$((i+3))
+done
+hem
+
+<< hi
+for (( i=3 ; i<=15 ; i+=3 ));
+do
+	echo "Prime No: $i"
+done
+hi
+
+<< comment
+for i in {1..50}
+do
+   echo "Welcome $i times"
+done
+commet
