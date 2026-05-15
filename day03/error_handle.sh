1dc9d83b96942828c544c0626e946e1c6a151d5e
hasnainbanusi <hasnain10.official@gmail.com>
2026-05-15 22:33:34 +0500

    hasnain

--- day03/error_handle.sh
+++ day03/error_handle.sh
@@ -4,10 +4,10 @@
 
 
 handle() {
-	mkdir demo
+	mkdir Demo #---> Here u can add anything (Name/Numbers etc)
 }
 
-if ! handle; then
+if ! handle; then #This portion will check error if exist in code 
 	echo "the code is exited as directory exist"
 	exit 1
 fi
